9475a884da24310d527f822806e8a3bdda593b44
Kevin Marinas <marinaskevin@outlook.com>
2019-11-12 20:25:44 -0800

    Created a new React project using create-react-app

---
+++ first-jsx/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'; // import the React library
+import ReactDOM from 'react-dom'; // import the ReactDOM library
+// we can create a simple element to display on our HTML page
+// const App = React.createElement("h1", null, "Hello World");
+
+// with JSX we can perform the same React.createElement() call with the following:
+const App = <h1>Hello World</h1>;
+
+// and load our App into the DOM, targeting the div located in /public/index.html
+ReactDOM.render(App, document.getElementById('root'));
