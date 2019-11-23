6596847073a3d55a8639db4dd2db9bd64c03e06d
Kevin Marinas <marinaskevin@outlook.com>
2019-11-22 23:13:05 -0800

    Added a counter to the Stateful Component project

--- stateful-component/src/components/App.js
+++ stateful-component/src/components/App.js
@@ -5,8 +5,14 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            text: 'Stateful component!'
+            text: 'Stateful component!',
+            count: 0
         }
+    }
+    updateCount() {
+       this.setState(prevState => ({
+            count: prevState.count + 1
+       }))
     }
     render(){
         return (
@@ -15,6 +21,11 @@ class App extends React.Component {
                 <button
                     onClick={ () => this.setState({ text: 'State has changed!' }) } >
                     Click to change state's text value!
+                </button>
+                <h1>{this.state.count}</h1>
+                <button
+                    onClick={ () => this.updateCount() } >
+                    +
                 </button>
             </div>
         )
